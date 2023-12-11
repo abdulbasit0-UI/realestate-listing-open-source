8ccdf61ab07d5ea2bd7b4c0516c57231c224a17b
Abdulbasit <habdulbasit099@gmail.com>
2023-12-11 19:57:38 +0200

    added navbar ui

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      alt="Logo"
+      src="/logo.png"
+      className="hidden md:block cursor-pointer"
+      height={100}
+      width={100}
+    />
+  );
+};
+
+export default Logo;
