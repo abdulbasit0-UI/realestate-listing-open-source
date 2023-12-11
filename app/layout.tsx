2999a1b6ae8819c4561bf7086376badef8ff492b
Abdulbasit <habdulbasit099@gmail.com>
2023-12-11 20:34:37 +0200

    working on auth modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/modal";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal  actionLabel="Submit" isOpen={true} />
         <Navbar />
         {children}
       </body>
