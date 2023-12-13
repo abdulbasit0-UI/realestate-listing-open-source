fb58b27eb882808a9f09aa4cd0216e7c8feee8ea
Abdulbasit <habdulbasit099@gmail.com>
2023-12-13 22:03:58 +0200

    finished listing card preview and favorite function

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import { toast } from "react-hot-toast";
+import { User } from "@prisma/client";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+
+interface UseFavorite {
+  listingId: string;
+  currentUser?: User | null;
+}
+
+const userFavorite = ({ listingId, currentUser }: UseFavorite) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+    return list.includes(listingId);
+  }, [currentUser]);
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      try {
+        let request;
+
+        if (hasFavorited) {
+          request = () => axios.delete(`/api/favorites/${listingId}`);
+        } else {
+          request = () => axios.post(`/api/favorites/${listingId}`);
+        }
+
+        await request();
+
+        router.refresh();
+        toast.success("success");
+      } catch (error) {
+        toast.error("Something went wrong");
+      }
+    },
+    [currentUser, hasFavorited, listingId, loginModal, router]
+  );
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
+
+export default userFavorite;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
 import Image from "next/image";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <>
+        <EmptyState showReset />
+      </>
+    );
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello airbnb
-    </main>
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: any) => {
+          return (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          );
+        })}
+      </div>
+    </Container>
   );
 }
